a3b42347dc54bb605271862ca18d77a845aafdc5
Sanjeev Halyal <1448731+sanjeevhalyal@users.noreply.github.com>
2025-07-12 17:26:15 +0100

    Add built lib directory

---
+++ lib/typescript/src/FallbackGlassEffectView.d.ts
@@ -0,0 +1,3 @@
+import type { GlassEffectViewProps } from "./GlassEffectViewNativeComponent";
+export declare const FallbackGlassEffectView: (props: GlassEffectViewProps) => import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=FallbackGlassEffectView.d.ts.map
