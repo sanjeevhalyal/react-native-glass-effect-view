37f1ee69da91ca19fb23254a29ff6742412086b3
Sanjeev Halyal <1448731+sanjeevhalyal@users.noreply.github.com>
2025-10-07 05:18:38 -0400

    update lib

--- lib/typescript/src/GlassEffectViewNativeComponent.d.ts
+++ lib/typescript/src/GlassEffectViewNativeComponent.d.ts
@@ -2,7 +2,6 @@ import type { ViewProps } from "react-native";
 import type { WithDefault } from "react-native/Libraries/Types/CodegenTypes";
 export type GlassEffectAppearance = "light" | "dark" | "default";
 export interface GlassEffectViewProps extends ViewProps {
-    isInteractive?: boolean;
     tintColor?: string;
     appearance?: WithDefault<GlassEffectAppearance, "default">;
     useNative?: WithDefault<boolean, true>;
